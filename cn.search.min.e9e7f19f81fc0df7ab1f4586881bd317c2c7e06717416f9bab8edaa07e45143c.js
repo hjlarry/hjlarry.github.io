cc062e8037a0fdfee296e48ecb0bedfab4071f7a
hjlarry <hjlarry@163.com>
2020-05-13 11:08:49 +0800

    add raft article view

--- cn.search.min.e9e7f19f81fc0df7ab1f4586881bd317c2c7e06717416f9bab8edaa07e45143c.js
+++ cn.search.min.e9e7f19f81fc0df7ab1f4586881bd317c2c7e06717416f9bab8edaa07e45143c.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/cn.search-data.min.4df43cc609658a3a50d0696ea5db62ff43c37c11c72d76c95afa10bac68bb407.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/cn.search-data.min.7f00b872e8047d2c9df344f34f4d10560cfe3e6d84507c42ac88c005dda2275c.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
